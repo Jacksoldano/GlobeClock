fecca1429aabe8cc0dadee3d167f0642e7d4a8f1
Jack Soldano <tazjack3@live.co.uk>
2025-04-03 22:49:01 +0100

    Update WiFi.ino to v2 code

--- XIAO_Globe_Clock_01/WiFi.ino
+++ XIAO_Globe_Clock_01/WiFi.ino
@@ -1,4 +1,22 @@
+/*****************************************************************************
+  Author:         Jack R Soldano
+  Release Date:   2025-04-01
+  Link:           Project details at https://github.com/Jacksoldano/GlobeClock
+  File Name:      WiFi.ino
+  Version:        02
+
+  Description
+  This Code is used in Globe Clock Project and uses an ESP32 XIAOC3
+  It gets the current time from the web then calculates the orientation
+  that the globe should be, effectively working out the current zenith of
+  the sun relative to the systems "zero" position.
+
+  - Just used for wifi setup
+*****************************************************************************/
+
 void wifiSETUP(){
+  // Generic wifi setup function
+  // Prints IP address of device and starts web server
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -8,7 +26,6 @@ void wifiSETUP(){
     Serial.print(".");
   }
   
-  // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.print("IP address: ");
